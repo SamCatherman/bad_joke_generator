0f58f2a5f090ea87a8bf48b3dcb36d6fa5876af8
SamCatherman <sam.catherman@gmail.com>
2020-10-06 20:11:59 -0700

    Add client application
    - create react app for UI
    - add procfile and rake task to run both dev servers simultaneously

---
+++ markov_dad_joke_api/client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button content="Click Here" />
+        <p>Dad joke goes here...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
