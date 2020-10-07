38cdaa6c48cad56e201d6949a383f7f2adae323c
SamCatherman <sam.catherman@gmail.com>
2020-10-06 20:42:23 -0700

    Fetch joke translation from client
    - Add simple interface for fetching and rendering a dad joke
    - Each time the button is clicked, a new joke is generated and rendered

--- markov_dad_joke_api/client/src/App.js
+++ markov_dad_joke_api/client/src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Button } from 'semantic-ui-react'
 
 function App() {
+  const [hasError, setErrors] = useState(false);
+  const [dadJoke, setDadJoke] = useState({});
+  
+  async function fetchData() {
+    const res = await fetch('http://localhost:3001/jokes/generate', { method: 'POST' })
+    res.json()
+       .then(res => setDadJoke(res))
+       .catch(err => setErrors(err))
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Button content="Click Here" />
-        <p>Dad joke goes here...</p>
+        <Button content="Click Here to generate a new Dad Joke" onClick={fetchData}/>
+        <p>{dadJoke.markov_translation}</p>
       </header>
     </div>
   );
